fc2a4203a43a9338f48b6a4e5f200690d7f2432c
Abhi Reddy <anagireddygari@bowdoin.edu>
2024-10-28 14:06:28 -0400

    Removed some imports

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -1,10 +1,6 @@
 "use client";
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import { SkillData } from "@/constants"; 
-import Image from "next/image";
-import { Autoplay } from "swiper/modules";
 
 const AboutMe = () => {
   return (
