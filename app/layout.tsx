9de1d3d77f9c992325a17a1948bded6881615f2d
Abhi Reddy <anagireddygari@bowdoin.edu>
2024-11-03 11:03:14 -0500

    Added vercel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Transition from "@/components/Transition";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -35,6 +36,7 @@ export default function RootLayout({
         <Transition />
         <div className="mt-8"> 
           {children}
+          <Analytics />
         </div>
         <Footer />
       </body>
